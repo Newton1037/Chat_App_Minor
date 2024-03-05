3f844fef8bd2f7d0e182815b40237db4165bb5d3
unknown <riteshsahoo58@gmail.com>
2024-03-06 01:16:38 +0530

    chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>vdfgf</h1>Home
-         
+      <Routes>
+       <Route path="/" Component={HomePage} exact />
+       <Route path="/chats" Component={ChatPage} />
+      </Routes>
+        
     </div>
   );
 }
