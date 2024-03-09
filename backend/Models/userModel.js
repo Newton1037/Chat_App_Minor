a65b6ba30a25d13742f7fe8e26f1134dcd7c6d2d
unknown <riteshsahoo58@gmail.com>
2024-03-10 02:16:46 +0530

    nn

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose")
 const UserModel = mongoose.Schema(
     {
        name: {type: String , required: true} , 
-       email: {type: String , required: true} , 
+       email: {type: String , required: true , unique: true} , 
        password: {type: String , required: true} , 
        pic: {type: String , 
-             required: true , 
              default: 
               "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
             } , 
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
 const express = require("express")
 const {chats} = require("./data/data")
+const connectDB = require("./config/db")
 const dotenv = require("dotenv")
+const colors = require("colors")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express()
 dotenv.config()
+connectDB()
 
 app.get( "/" , (req,res) => {
     res.send("API is running")
 })
 
-app.get( "/api/chat" , (req,res) => {
-    res.send(chats)
-})
-
-app.get( "/api/chat/:id" , (req,res) => {
-    const single = chats.find((c) => c._id === req.params.id)
-    res.send(single)
-})
+app.use("/api/user" , userRoutes)
 
 PORT = process.env.PORT || 5000
 
-app.listen(`${PORT}` , console.log(`Server started on port ${PORT}`))
+app.listen(`${PORT}` , console.log(`Server started on port ${PORT}`.yellow.bold))
